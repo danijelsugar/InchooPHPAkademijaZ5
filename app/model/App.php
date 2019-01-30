3a3a67570716e665066aad04507b2999fcc6f796
Danijel Šugar <danijel.sugar@outlook.com>
2019-01-30 19:07:25 +0100

    Resolved controllers and actions

--- app/model/App.php
+++ app/model/App.php
@@ -8,7 +8,34 @@ final class App
 
     public static function start()
     {
-        echo "evo me";
+        $pathInfo = Request::pathInfo();
+        $pathInfo = trim($pathInfo,'/');
+        $pathParts = explode('/',$pathInfo);
+        print_r($pathParts);
+
+        //resolve controller
+        if (!isset($pathParts[0])) {
+            $controller = 'Index';
+        } else {
+            $controller = ucfirst(strtolower($pathParts[0]));
+        }
+
+        $controller .= 'Controller';
+
+        //resolve action
+        if (!isset($pathParts[1])) {
+            $action = 'index';
+        } else {
+            $action = strtolower($pathParts[1]);
+        }
+
+        //dispatch
+        if (class_exists($controller) && method_exists($controller, $action)) {
+            $controllerInstance = new $controller();
+            $controllerInstance->$action();
+        } else {
+            header("HTTP/1.0 404 Not Found");
+        }
     }
 
 }
