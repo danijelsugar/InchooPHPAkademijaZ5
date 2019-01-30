a010d8d4069d4b2ce195e5fbd792bf70c509c91b
Danijel Šugar <danijel.sugar@outlook.com>
2019-01-30 18:00:16 +0100

    App class for resolving controllers and actions,request class handles requests

---
+++ app/model/App.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class App decides which controller and action should be used
+ */
+final class App
+{
+
+    public static function start()
+    {
+        echo "evo me";
+    }
+
+}
