b4d289be4cb21c13ad21d5f4f97c6485d4f62010
Danijel Šugar <danijel.sugar@outlook.com>
2019-01-30 16:58:56 +0100

    Autoloader for including classes

--- index.php
+++ index.php
@@ -7,3 +7,20 @@ define('BP', __DIR__ . 'index.php/');
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 
+//path were included classes would be find
+$includePaths = implode(DIRECTORY_SEPARATOR, [
+    BP . 'app/model',
+    BP . 'app/controller'
+]);
+
+set_include_path($includePaths);
+
+//register autoloader, to auto include classes when needed
+spl_autoload_register(function($class)
+{
+    $classPath = strtr($class, '\\', DIRECTORY_SEPARATOR) . '.php';
+
+    return include $classPath;
+});
+
+
